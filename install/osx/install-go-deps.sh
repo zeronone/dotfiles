4b5b716bbb69a10baddfb935f96a4e1ae948ead5
Arif Rezai <majafer10@gmail.com>
2019-11-20 18:13:08 +0900

    Updated

--- install/osx/install-go-deps.sh
+++ install/osx/install-go-deps.sh
@@ -5,16 +5,16 @@ brew uninstall go --force
 # install gvm
 curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | zsh
 source $HOME/.gvm/scripts/gvm
-gvm install go1.12.10 -B && gvm use go1.12.10 --default
+gvm install go1.13.4 -B && gvm use go1.13.4 --default
 
 go version
 
-go get -u github.com/motemen/gore
+go get -u github.com/motemen/gore/cmd/gore
 go get -u github.com/mdempsky/gocode
 go get -u golang.org/x/tools/cmd/godoc
 go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/tools/cmd/gorename
 go get -u golang.org/x/tools/cmd/guru
-go get -u github.com/nathany/looper
-go get -v github.com/rogpeppe/godef
+go get -u github.com/cweill/gotests/...
+go get -u github.com/fatih/gomodifytags
 
