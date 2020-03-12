e34b7f661c29e7ffca8fe41b563536e4ab8445de
Arif Rezai <majafer10@gmail.com>
2020-03-12 10:40:26 +0900

    Updated

--- install/osx/tweak-keyboard.sh
+++ install/osx/tweak-keyboard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ## needs restart
-defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
+defaults write -g InitialKeyRepeat -int 20 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
